a52cb7645732768b3113a8ff8c2061c72ddcb115
felipe rojas <felipe.rojas.g@mail.pucv.cl>
2020-09-07 05:38:50 -0300

    cambio2

--- diccionario.py
+++ diccionario.py
@@ -1,1 +1,4 @@
 # diccionario
+
+diccionario = {'nombres':['ignacio', 'nestor', 'matias' 'flavio'], 'apellidos':['flores', 'carvacho', 'figura', 'alejos'], 'cursos':['programacion', 'fisica'], 'notas':[[4.5,6.8], [3.0,5.5], [7.0,6.6], [5.5,4.5]]}
+
