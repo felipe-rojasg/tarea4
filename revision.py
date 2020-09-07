dfebf5397d9d67fc76b0370a8c13753f5c388184
felipe rojas <felipe.rojas.g@mail.pucv.cl>
2020-09-07 04:59:02 -0300

    cambio3

--- revision.py
+++ revision.py
@@ -1,6 +1,10 @@
 # revision
-x=4
-y=11
+x=11
+y=4
 
 print (f'la suma es, x+y={x}+{y}={x+y]')
 print (f'la resta es, x-y={x}-{y}=[x-y]')
+
+print (f'la multiplicacion es, x*y={x}*{y}={x*y}')
+print (f'la division es, x/y={x}/{y}={x//y}')
+print (f'el modulo es, {x%y}'}
