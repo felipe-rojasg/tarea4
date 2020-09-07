409c8d86a1d7dddc564a64ccfa63c17182ab0adf
felipe rojas <felipe.rojas.g@mail.pucv.cl>
2020-09-07 05:51:00 -0300

    cambio3

--- diccionario.py
+++ diccionario.py
@@ -1,4 +1,10 @@
 # diccionario
 
-diccionario = {'nombres':['ignacio', 'nestor', 'matias' 'flavio'], 'apellidos':['flores', 'carvacho', 'figura', 'alejos'], 'cursos':['programacion', 'fisica'], 'notas':[[4.5,6.8], [3.0,5.5], [7.0,6.6], [5.5,4.5]]}
+diccionario = {'nombre':['ignacio', 'nestor', 'matias' 'flavio'], 'apellido':['flores', 'carvacho', 'figura', 'alejos'], 'cursos':['programacion', 'fisica'], 'notas':[[4.5,6.8], [3.0,5.5], [7.0,6.6], [5.5,4.5]]}
 
+print (diccionario['nombre'][2])
+print (diccionario['apellido'[2])
+print (diccionario['cursos'[0])
+print (diccionario['notas'[2][0])
+print (diccionario['cursos'[1])
+print (diccionario['notas'[2][1])
